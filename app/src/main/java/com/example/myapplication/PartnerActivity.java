ab3b209077693b74f978997dbe997795c099ccbe
ouzmane1 <ndomousmane24@gmail.com>
2024-06-18 20:52:50 +0200

    modifie commit

--- app/src/main/java/com/example/myapplication/PartnerActivity.java
+++ app/src/main/java/com/example/myapplication/PartnerActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+//import static android.os.Build.VERSION_CODES.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -23,6 +25,53 @@ public class PartnerActivity extends AppCompatActivity {
 
         jArray=null;
         EditText rech = (findViewById(R.id.rech));
+        findViewById(R.id.recherche).setOnClickListener(view -> {
+            try {
+                String adresse = "http://192.168.1.106/test.php?rech="+rech.getText().toString();
+                String wololo = new HTTPRequest.HTTPSELECTRequest().execute(adresse).get();
+                jArray = new JSONArray(wololo);
+                i=0;
+                json_data = jArray.getJSONObject(i);
+                ((TextView) findViewById(R.id.name1)).setText(json_data.getString("name"));
+                ((TextView) findViewById(R.id.street)).setText(json_data.getString("street"));
+                ((TextView) findViewById(R.id.mail)).setText(json_data.getString("email"));
+                ((TextView) findViewById(R.id.phone)).setText(json_data.getString("phone"));
+                ((TextView) findViewById(R.id.fonction)).setText(json_data.getString("function"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(view -> {
+            try {
+                if(i>0) {
+                    i--;
+                    json_data = jArray.getJSONObject(i);
+                    ((TextView) findViewById(R.id.name1)).setText(json_data.getString("name"));
+                    ((TextView) findViewById(R.id.street)).setText(json_data.getString("street"));
+                    ((TextView) findViewById(R.id.mail)).setText(json_data.getString("email"));
+                    ((TextView) findViewById(R.id.phone)).setText(json_data.getString("phone"));
+                    ((TextView) findViewById(R.id.fonction)).setText(json_data.getString("function"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(view -> {
+            try {
+                if(i<jArray.length()-1) {
+                    i++;
+                    json_data = jArray.getJSONObject(i);
+                    ((TextView) findViewById(R.id.name1)).setText(json_data.getString("name"));
+                    ((TextView) findViewById(R.id.street)).setText(json_data.getString("street"));
+                    ((TextView) findViewById(R.id.mail)).setText(json_data.getString("email"));
+                    ((TextView) findViewById(R.id.phone)).setText(json_data.getString("phone"));
+                    ((TextView) findViewById(R.id.fonction)).setText(json_data.getString("function"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
         findViewById(R.id.button).setOnClickListener(view -> {
             try {
                 String adresse = "http://192.168.1.106/test.php?rech="+rech.getText().toString();
